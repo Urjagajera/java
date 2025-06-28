545ce4c1ff72e083f7e583defc55adaca3a09870
Urjagajera <urjagajera12@gmail.com>
2025-06-28 17:07:14 +0530

    string_length.java

---
+++ string_length.java
@@ -0,0 +1,10 @@
+public class First {
+    public static void main(String[] args) {
+
+        String name = "urja Gajera";
+
+        System.out.println(name.length());
+        
+        //output: 11
+    }
+}
