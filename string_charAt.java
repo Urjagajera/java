be44071c2097cf0e41d7886e8989d4e869b75b47
Urjagajera <urjagajera12@gmail.com>
2025-06-28 17:12:53 +0530

    string_charAt.java

---
+++ string_charAt.java
@@ -0,0 +1,10 @@
+public class First {
+    public static void main(String[] args) {
+
+        String name = "urja Gajera";
+
+        System.out.println(name.charAt(3));
+        
+      // output : a
+    }
+}
