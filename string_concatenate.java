a3ba69245e76f5183456a773fa9246ff1bbab4c2
Urjagajera <urjagajera12@gmail.com>
2025-06-28 16:58:21 +0530

    string_concatenate.java

---
+++ string_concatenate.java
@@ -0,0 +1,11 @@
+public class First {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+
+        String name = "urja Gajera";
+        
+        System.out.println("My name is " + name );
+        
+        
+    }
+}
