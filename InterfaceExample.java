659efc8bb92ba019855d58342aecf3e9b642796f
Urjagajera <urjagajera12@gmail.com>
2026-01-13 10:26:12 +0530

    Implement Vehicle interface with Car class

---
+++ InterfaceExample.java
@@ -0,0 +1,16 @@
+interface Vehicle {
+    void start();
+}
+
+class Car implements Vehicle {
+    public void start() {
+        System.out.println("Car starts");
+    }
+}
+
+public class InterfaceExample {
+    public static void main(String[] args) {
+        Vehicle v = new Car();
+        v.start();
+    }
+}
