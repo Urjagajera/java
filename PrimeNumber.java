9c7a3150220961060ddcb2130565c59b3336f163
Urjagajera <urjagajera12@gmail.com>
2026-01-13 10:33:43 +0530

    Add PrimeNumber class to check for prime numbers

---
+++ PrimeNumber.java
@@ -0,0 +1,12 @@
+public class PrimeNumber {
+    public static void main(String[] args) {
+        int n = 17, flag = 0;
+        for (int i = 2; i <= n / 2; i++) {
+            if (n % i == 0) {
+                flag = 1;
+                break;
+            }
+        }
+        System.out.println(flag == 0 ? "Prime" : "Not Prime");
+    }
+}
